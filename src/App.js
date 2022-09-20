28ec7849b77fb25a4fe8c1c1d1c0a3292baf719e
maferojedac <103955626+maferojedac@users.noreply.github.com>
2022-09-20 03:03:53 -0500

    Resultado final

--- src/App.js
+++ src/App.js
@@ -2,124 +2,109 @@ import {React, useState} from 'react';
 import {v4 as uuid} from 'uuid';
 /* import TextField from "@mui/material/TextField"; */
 import './App.css';
-import {List} from './components/List';
-import {Task} from './components/Task';
+/* import List from './components/List'; */
 
 function App()
 {
-/* 
-  /* para poner las palomitas de la lista 
-  const [check, setCheck] = useState([]) */
 
-   /* para que el usuario escriba */
-   const [userInput, setUserInput] = useState("");
-   const [changeInput, setChangeInput] = useState("");
+  /* para poner las palomitas de la lista */
+  const [check, setCheck] = useState([])
 
   /*arreglo de lista para la lista por default*/
+  const [defaultList, setDefaultList] = useState
+  (
+    [ {id: uuid(),
+      item: 'Feed dog',
+      check: false},
 
-  const defaultList = [ 
-    {id: uuid(),
-    item: 'Feed dog',
-    check: false},
+      {id: uuid(),
+      item: 'Finish homework',
+      check: false},
 
-    {id: uuid(),
-    item: 'Finish homework',
-    check: false},
+      {id: uuid(),
+      item: 'Help mom',
+      check: false} ]
+  )
 
-    {id: uuid(),
-    item: 'Help mom',
-    check: false} 
-  ];
-
-  const [defaultTasks, setDefaultList] = useState(defaultList);
-    
-  let searchTask;
-
-  if(userInput < 1) {
-    searchTask = defaultTasks;
-  } else {
-    searchTask = defaultTasks.filter(task => {
-      const itemText = task.item.toLowerCase();
-      const inputText = userInput.toLowerCase();
-
-      return itemText.includes(inputText);
-    })
-  }
  
   /* componente para borrar las tareas */
-  const deleteTask = () => 
+  const deleteTask = (id) => 
   {
-    const indexTask = defaultTasks.findIndex(task =>  {
-      const itemText = task.item.toLowerCase();
-      const inputText = changeInput.toLowerCase();
-      return itemText === inputText;
-    });
-    const newTasks = [...defaultTasks];
-    
-    if(indexTask!= -1) {
-      newTasks.splice(indexTask, 1);
-      setDefaultList(newTasks);
-    } 
+    const list = defaultList.filter((item) => item.id !== id)
+    console.log(id)
+
+    setDefaultList(list)
   }
 
   
- 
-
-  const inputChange = (event) => {
-    setUserInput(event.target.value);
-  }
-
-  const inputModifyChange = (event) => {
-    setChangeInput(event.target.value);
-  }
-
+  /* para que el usuario escriba */
+  const [userInput, setUserInput] = useState("")
 
 
   /* componente para agregar tareas */
   const addTask = () =>
   {
+    /* objeto de una nueva tarea */
+    const newTask = 
+    {
+      id: uuid(),
+      content: userInput 
+    }
+
     /* para agregar las nuevas tareas */
-    const newTasks = [...defaultTasks];
-    newTasks.push(
-      {id: uuid(),
-        item: changeInput,
-        check: false
-      }
-    );
+    setDefaultList([...defaultList, newTask])
+
     /* para limpiar el input una vez creada la tarea */
-    setDefaultList(newTasks);
+    setUserInput("")
   }
 
-  /* para buscar tareas*/
-    
-  return (
-    <>
-      
-      <h1>To-do List</h1>
-      <input
-        type="text"
-        placeholder="Buscador"
-        value={userInput}
-        onChange={inputChange}
-      />
-      <input
-        type="text"
-        placeholder="Agregar Borrar"
-        value={changeInput}
-        onChange={inputModifyChange}
-      />
-      <button onClick={addTask}> + </button>
-      <button onClick={deleteTask}> Delete </button>
 
-      <List>
-        {searchTask.map(todo => (
-            <Task
-                key={todo.id}
-                text={todo.item}
-            />
-        ))}
-    </List>
-    </>
+  /* para buscar tareas*/
+  const searchTask = () =>
+  {
+
+    const buscado = defaultList.filter((item) =>
+      {
+        if(searchTask == "") {return item}
+        else if(item.content.toLowerCase().includes(searchTask.toLowerCase()))  {return item}
+      })
+
+      return (
+        <ul>
+          {buscado.map((item) => ( <li key={item.id}> {item.content} </li> ))}
+        </ul>
+      )
+  }
+
+  
+  return (
+    <div className='main_div'>
+      <div className='center_div'>
+        <h1>To-do List</h1>
+
+          <input
+            type="text"
+            placeholder="Escribe tu tarea"
+            value={userInput}
+            onChange={(event) => setUserInput(event.target.value)}>
+          </input>
+
+          <button onClick={() => addTask()}> + </button>
+          <button onClick={() => searchTask()}> S </button>
+          <button onClick={() => deleteTask()}> D </button>
+
+          {/* <List/> */}
+
+          {/* para imprimir la lista */}
+          <ul>
+            {defaultList.map((userInput) => 
+            (
+              <li key={userInput.id}> {userInput.content} </li> 
+            ))}
+            
+          </ul>
+      </div>
+    </div>
   )
 }
 export default App
